5edec6688c9ec6617dd36a675026908361697334
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-04 23:24:50 +0530

    [Muskan] Added : Fisrt file for user registration problem.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name:");
+            string firstName = Console.ReadLine();
+
+           
+        }
+    }
+}
